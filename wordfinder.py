88b58a503501b91fa8e8ad695d57709d4b9218fc
S T <itschibits@gmail.com>
2021-04-20 19:44:26 -0400

    created random word generator

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,22 @@
+import random
+
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.words = open(f"{file_path}", "r").read()
+        self.word_list = self.words.split("\n")
+        print(len(self.word_list), "words read")
+
+    def random(self):
+        return random.choice(self.word_list)
+
+
+class SpecialWordFinder(WordFinder):
+    """Special Word Finder: finds random words in a file with hash tags
+    or empty new lines"""
+    def __init__(self, file_path):
+        super().__init__(file_path)
+        self.word_list = [word for word in self.word_list
+                          if not word.startswith("#") and word]
